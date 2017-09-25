530b542bdf15e086125633738d34517bb01a587e
sindunuragarp <sindunuragarp@gmail.com>
2017-09-25 11:25:10 +0200

    created basic string splitting

--- Ex1/WordFreqCounts.scala
+++ Ex1/WordFreqCounts.scala
@@ -7,23 +7,54 @@ import java.io._
 import java.util.Locale
 import org.apache.commons.lang3.StringUtils
 
-object WordFreqCounts 
-{
-	def main(args: Array[String]) 
-	{
-		val inputFile = args(0) // Get input file's name from this command line argument
-		val conf = new SparkConf().setAppName("WordFreqCounts")
-		val sc = new SparkContext(conf)
-		
+object WordFreqCounts {
+
+  val conf = new SparkConf().setAppName("WordFreqCounts")
+  val sc = new SparkContext(conf)
+
+  def main(args: Array[String]): Unit = {
+
+    // Get input file's name from this command line argument
+		val inputFile = args(0)
 		println("Input file: " + inputFile)
-		
+
 		// Uncomment these two lines if you want to see a less verbose messages from Spark
-		//Logger.getLogger("org").setLevel(Level.OFF);
-		//Logger.getLogger("akka").setLevel(Level.OFF);
-		
+		Logger.getLogger("org").setLevel(Level.OFF);
+		Logger.getLogger("akka").setLevel(Level.OFF);
+
+    // Run and time program
 		val t0 = System.currentTimeMillis
-		
+    extractWords(inputFile)
 		val et = (System.currentTimeMillis - t0) / 1000
-		System.err.println("Done!\nTime taken = %d mins %d secs".format(et / 60, et % 60))
+
+    System.err.println("Done!\nTime taken = %d mins %d secs".format(et / 60, et % 60))
 	}
+
+  ////
+
+  def extractWords(inputFile: String): Unit = {
+    val wordRegex = "[a-zA-Z]\*[a-zA-Z]".r()
+
+    val rdd_text = sc.textfile(inputFile)
+    val words = rdd_text
+      .map(x => x.split(" "))
+      .map(_.toLowerCase)
+
+    writeToFile(words, "freq.txt")
+  }
+
+  ////
+
+  def writeToFile(content: Array[String], filePath: String): Unit = {
+    new File("output").mkdirs
+    val pw = new java.io.PrintWriter(new File(filePath))
+
+    try {
+      pw.write("Word\n")
+      content.foreach(line => pw.write(line + "\n"))
+    }
+    finally {
+      pw.close
+    }
+  }
 }
