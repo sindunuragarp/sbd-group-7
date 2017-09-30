47fde82c70beb2082cf9a309053cf46a0f1416ed
sindunuragarp <sindunuragarp@gmail.com>
2017-09-30 20:19:46 +0200

    Extracted tweet content and reduced by window and key

--- Lab1/Ex2/Twitterstats.scala
+++ Lab1/Ex2/Twitterstats.scala
@@ -15,12 +15,45 @@ import twitter4j._
 // Check example https://github.com/apache/bahir/blob/master/streaming-twitter/examples/src/main/scala/org/apache/spark/examples/streaming/twitter/TwitterPopularTags.scala
 
 object Twitterstats {
-  
+
+  def getTimeStamp: String = {
+    new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime)
+  }
+
+  def getLangNameFromCode(code: String) : String = {
+    new Locale(code).getDisplayLanguage(Locale.ENGLISH)
+  }
+
+  def getLang(s: String) : String = {
+    val inputStr = s.replaceFirst("RT", "").replaceAll("@\\p{L}+", "").replaceAll("https?://\\S+\\s?", "")
+    var langCode = new LanguageIdentifier(inputStr).getLanguage
+
+    // Detect if japanese
+    var pat = Pattern.compile("\\p{InHiragana}")
+    var m = pat.matcher(inputStr)
+    if (langCode == "lt" && m.find)
+      langCode = "ja"
+
+    // Detect if korean
+    pat = Pattern.compile("\\p{IsHangul}")
+    m = pat.matcher(inputStr)
+    if (langCode == "lt" && m.find)
+      langCode = "ko"
+
+    return langCode
+  }
+
+  def isEnglish(s: String): Boolean = {
+    getLang(s) == "en" || getLang(s) == "no"
+  }
+
+  ////
+
 	def main(args: Array[String]): Unit = {
 		val file = new File("cred.xml")
 		val documentBuilderFactory = DocumentBuilderFactory.newInstance
 		val documentBuilder = documentBuilderFactory.newDocumentBuilder
-		val document = documentBuilder.parse(file);
+		val document = documentBuilder.parse(file)
 			
 		// Configure Twitter credentials
 		val consumerKey = document.getElementsByTagName("consumerKey").item(0).getTextContent 				
@@ -46,43 +79,71 @@ object Twitterstats {
 		val stream = TwitterUtils.createStream(ssc, None)
 		
 		// Start stream processing
-		handleTweetStream(stream);
+		handleTweetStream(stream)
 		ssc.start()
 		ssc.awaitTermination()
 	}
 
   ////
 
-  def handleTweetStream(stream: DStream[Status]): Unit = {
+  def handleTweetStream(tweets: DStream[Status]): Unit = {
 
+    // only take english tweets from stream
+    val englishTweets = tweets.filter(x => isEnglish(x.getText))
+
+    // extract target information from tweets
+    val tweetInfos = englishTweets.map(x => extractStatus(x))
+
+    // group tweets for every time window
+    val tweetBuckets = tweetInfos.reduceByKeyAndWindow( (a: (String, String, Array[String], Int, Int), b: (String, String, Array[String], Int, Int)) => (
+      a._1,
+      a._2,
+      a._3,
+      Math.max(a._4, a._5),
+      Math.min(a._4, a._5)
+    ), Seconds(120), Seconds(2))
+
+    tweetBuckets.foreachRDD(rdd => rdd.foreach(x => println(x)))
+  }
+
+  def extractStatus(status: Status): (Long, (String, String, Array[String], Int, Int)) = {
+    val x =
+      if (status.isRetweet) status.getRetweetedStatus
+      else status
+
+    val tweetCount = x.getRetweetCount + 1
+
+    (x.getId, (
+      x.getUser.getScreenName,
+      x.getText,
+      x.getHashtagEntities.map(_.getText),
+      tweetCount,
+      tweetCount
+    ))
   }
 
   ////
 
-  def getTimeStamp() : String = {
-    return new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime())
-  }
+  def writeToFile(content: Iterable[String], filePath: String): Unit = {
+    val outDir = "output"
+    new File(outDir).mkdirs
+    val writer = new java.io.PrintWriter(
+      new FileOutputStream(
+        new File(outDir + "/" + filePath),
+        true // append to file
+      )
+    )
 
-  def getLangNameFromCode(code: String) : String = {
-    return new Locale(code).getDisplayLanguage(Locale.ENGLISH)
-  }
-
-  def getLang(s: String) : String = {
-    val inputStr = s.replaceFirst("RT", "").replaceAll("@\\p{L}+", "").replaceAll("https?://\\S+\\s?", "")
-    var langCode = new LanguageIdentifier(inputStr).getLanguage
-
-    // Detect if japanese
-    var pat = Pattern.compile("\\p{InHiragana}")
-    var m = pat.matcher(inputStr)
-    if (langCode == "lt" && m.find)
-      langCode = "ja"
-    // Detect if korean
-    pat = Pattern.compile("\\p{IsHangul}");
-    m = pat.matcher(inputStr)
-    if (langCode == "lt" && m.find)
-      langCode = "ko"
-
-    return langCode
+    try {
+      content
+        .foreach(x => {
+          writer.append(x)
+          writer.append("Record#. <Number of HashTags for this HashTag> #<HashTag>:<user_name>:<tweetCount> < tweetText>\n")
+        })
+    }
+    finally {
+      writer.close()
+    }
   }
 }
 
