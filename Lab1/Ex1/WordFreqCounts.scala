6ffdbc0d8c71ffaca5504382a012ba3d163389ab
sindunuragarp <sindunuragarp@gmail.com>
2017-09-30 09:04:57 +0200

    Added more details into comments + returned splitting of text by line

--- Lab1/Ex1/WordFreqCounts.scala
+++ Lab1/Ex1/WordFreqCounts.scala
@@ -15,7 +15,7 @@ object WordFreqCounts {
 
   def main(args: Array[String]): Unit = {
 
-    // Get input file's name from this command line argument
+    // Get input file's name from the command line argument
 		val inputFile = args(0)
 		println("Input file: " + inputFile)
 
@@ -38,42 +38,43 @@ object WordFreqCounts {
     // Custom regex for splitting text into words (as defined) and non words
     val wordRegex = """([a-zA-Z][\w']*-?[a-zA-Z]+|[a-zA-Z])|([^a-zA-Z\s])+""".r()
 
-    // Read input file
-    // texts = (line)
-    val texts = sc.textFile(inputFile)
+    // Read input file as rdd, and split by carriage return to get each line
+    // >> lines = (line)
+    val lines = sc.textFile(inputFile)
+      .flatMap(x => x.split("""\n\r"""))
 
     // Split each line into individual words according to the word regex
-    // words = (word)
-    val words = texts
-      .map(_.toLowerCase)
-      .map("." + _)
+    // >> words = [(word)]
+    val words = lines
+      .map(_.toLowerCase) // convert to lower case to avoid case sensitivity
+      .map("." + _)       // added full stop at start of each line (no prev word after a newline)
       .flatMap(x => wordRegex.findAllIn(x));
 
     // Transform as pairs of current and previous word, then convert into tuple format
-    // wordPairs = ((word, preceded word))
+    // >> wordPairs = [((word, preceded word), 1)]
     val wordPairs = words.sliding(2)
       .map(x => ( (x(1),x(0)), 1 ))
 
-    // Reduce by key to get frequency of pairs
-    // pairsFreq = ((word, preceded word), frequency)
+    // Reduce by pair tuple to get frequency of pairs
+    // >> pairsFreq = [((word, preceded word), frequency)]
     val pairsFreq = wordPairs
       .filter(x => isWord(x._1._1))
       .reduceByKey(_ + _)
 
     // Reduce by first word to get frequency of curr word
-    // currWordsFreq = (word, frequency)
+    // >> currWordsFreq = [(word, frequency)]
     val currWordsFreq = pairsFreq
       .map(x => (x._1._1,x._2))
       .reduceByKey(_ + _)
 
-    // Filter non words to get frequency of prev word
-    // prevWordsFreq = (word, (previous word, frequency)
+    // Filter non words to get frequency of actual prev words, and transform to have curr word as key
+    // >> prevWordsFreq = [(word, (previous word, frequency)]
     val prevWordsFreq = pairsFreq
       .filter(x => isWord(x._1._2))
       .map(x => ( x._1._1, (x._1._2, x._2) ))
 
     // Combine frequency of curr and prev word for printing
-    // wordsCount = (word, (frequency of the word, (previous word, frequency of previous word))
+    // >> wordsCount = [(word, (frequency of word, [(previous word, frequency of previous word)] )]
     val wordsCount = currWordsFreq.cogroup(prevWordsFreq)
 
     // Write to output
@@ -82,10 +83,12 @@ object WordFreqCounts {
 
   ////
 
+  // because of the regex split, all string starting with a letter can be considered a word
   def isWord(text: String): Boolean = {
     return text.charAt(0).isLetter
   }
 
+  // sorts the iterables by freq and alphabet, and prints them to the specified output file (inside the output dir)
   def writeToFile(content: Iterable[(String, (Iterable[Int], Iterable[(String, Int)]))], filePath: String): Unit = {
     val outDir = "output"
     new File(outDir).mkdirs
