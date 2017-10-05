ae3c8b33d893276c0a87268a88e068e4b86b7b8d
sindunuragarp <sindunuragarp@gmail.com>
2017-10-05 16:42:52 +0200

    Added originial code for Lab2

---
+++ Lab2/code/get_dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+INSTALL_COMMAND="sudo pip-3.4 install"
+dependencies="warcio requests requests_file boto3 botocore py4j spark"
+
+for dep in $dependencies; do
+    $INSTALL_COMMAND $dep
+done;
+
