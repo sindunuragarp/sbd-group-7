ec43d0921b164918e6c8486e00f4083cf74ede9a
sindunuragarp <sindunuragarp@gmail.com>
2017-10-09 15:14:53 +0200

    Now reads directory from argument

--- Lab2/code/read_parquet.py
+++ Lab2/code/read_parquet.py
@@ -7,16 +7,28 @@ Created on Sun Oct  8 17:43:54 2017
 @author: sindunuragarp
 """
 
+import argparse
 import os
 import pandas as pd
 from fastparquet import ParquetFile
 
 ###############################################################################
 
-directory = "data/output/"
-files = [directory + f for f in os.listdir(directory) if f.endswith('.parquet')]
+parser = argparse.ArgumentParser("Read a folder of parquet files")
+parser.add_argument("--input", '-i', metavar="segment_index",
+                    type=str, required=True,
+                    help="path to directory")
+                    
+conf = parser.parse_args()
+
+###############################################################################
+
+directory = conf.input
+if directory is None:
+    directory = "data/output/"
 
 frames = []
+files = [directory + f for f in os.listdir(directory) if f.endswith('.parquet')]
 for file in files:
     pf = ParquetFile(file)
     df = pf.to_pandas()
